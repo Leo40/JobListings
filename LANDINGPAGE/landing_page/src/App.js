1cf37a3a1318a4d3e6245a4c482be83e648687b9
Mustafa <myasad91@gmail.com>
2021-10-08 23:49:38 +0300

    Finished hero + navbar components

---
+++ LANDINGPAGE/landing_page/src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      
+      //Features
+      //Extensions
+      //FAQs
+      //Footer
+      </>
+  );
+}
+
+export default App;
